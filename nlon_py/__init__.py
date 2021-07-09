84c2b62c3bdf50b60b4ff8cce94a9e0fbb81286e
Jianwen-Xu <xujianwen37@gmail.com>
2021-07-10 00:24:32 +0300

    Update init.py

--- nlon_py/__init__.py
+++ nlon_py/__init__.py
@@ -4,9 +4,9 @@ __author__ = """Jianwen Xu"""
 __email__ = 'xujianwen37@gmail.com'
 __version__ = '0.1.1'
 
-from model import NLoNModel
-from data.make_data import loadDataFromFiles
-from features import Character3Grams
+from nlon_py.model import NLoNModel
+from nlon_py.data.make_data import loadDataFromFiles
+from nlon_py.features import Character3Grams
 X, y = loadDataFromFiles()
 clf = NLoNModel(Character3Grams(X), y, nfolds=10)
 print(clf)
