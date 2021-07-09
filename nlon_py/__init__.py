c1f752d15655f97988d0c238cf9bc7e5e48488fa
Jianwen-Xu <xujianwen37@gmail.com>
2021-07-10 00:43:05 +0300

    Comment the test code

--- nlon_py/__init__.py
+++ nlon_py/__init__.py
@@ -7,6 +7,6 @@ __version__ = '0.1.1'
 from nlon_py.model import NLoNModel
 from nlon_py.data.make_data import loadDataFromFiles
 from nlon_py.features import Character3Grams
-X, y = loadDataFromFiles()
-clf = NLoNModel(Character3Grams(X), y, nfolds=10)
-print(clf)
+# X, y = loadDataFromFiles()
+# clf = NLoNModel(Character3Grams(X), y, nfolds=10)
+# print(clf)
