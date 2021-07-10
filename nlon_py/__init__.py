1c69622e952e7f4e2d2d92587478d22a0498da0a
Jianwen-Xu <xujianwen37@gmail.com>
2021-07-10 23:29:08 +0300

    Add stop words and cross validation

--- nlon_py/__init__.py
+++ nlon_py/__init__.py
@@ -4,9 +4,10 @@ __author__ = """Jianwen Xu"""
 __email__ = 'xujianwen37@gmail.com'
 __version__ = '0.1.1'
 
-from nlon_py.model import NLoNModel
 from nlon_py.data.make_data import loadDataFromFiles
 from nlon_py.features import Character3Grams
+from nlon_py.model import CompareModels, NLoNModel
+
 # X, y = loadDataFromFiles()
 # clf = NLoNModel(Character3Grams(X), y, nfolds=10)
-# print(clf)
+# CompareModels(Character3Grams(X), y)
--- nlon_py/features.py
+++ nlon_py/features.py
@@ -1,9 +1,20 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
+from nlon_py.data.make_data import loadStopWords
+
+stop_words_list = loadStopWords()
+
 
 def Character3Grams(text):
-
     trigram_vectorizer = CountVectorizer(
-        ngram_range=(3, 3), token_pattern=r'\b\w+\b', min_df=1)
-    analyze = trigram_vectorizer.build_analyzer()
+        ngram_range=(3, 3), stop_words=preprocess(stop_words_list))
     return trigram_vectorizer.fit_transform(text).toarray()
+
+
+def preprocess(text):
+    vectorizer = CountVectorizer()
+    analyze = vectorizer.build_analyzer()
+    result = []
+    for x in text:
+        result.extend(analyze(x))
+    return result
