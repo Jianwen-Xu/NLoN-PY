39acd86cd05f0b8dbe848ebf3391bb5fa3ff30ed
Jianwen Xu <xujianwen37@gmail.com>
2026-04-25 10:00:23 +0300

    fix: remove deprecated numpy.core.fromnumeric import in model.py

--- nlon_py/model.py
+++ nlon_py/model.py
@@ -4,7 +4,6 @@ from joblib.logger import PrintTime
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.core.fromnumeric import shape
 import pandas as pd
 import seaborn as sns
 # explicitly require this experimental feature
